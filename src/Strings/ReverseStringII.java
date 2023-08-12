cd7fe829731b3ada0709fff581d923488273c3bb
akshay-03 <agarwal3akshay@gmail.com>
2023-08-12 23:12:48 +0530

    reverse-string-II-problem

---
+++ src/Strings/ReverseStringII.java
@@ -0,0 +1,10 @@
+package Strings;
+
+public class ReverseStringII {
+
+    //given a string, reverse it and remove all the leading and trailing spaces.
+
+    // example: String str = " Aadhi programs in Java   "
+
+    // result -> "Java in programs Aadhi"
+}
