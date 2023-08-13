5d6744992d9edf78ee4a8d6435b9b7b0df20b4ef
unknown <aadhisuresh01@gmail.com>
2023-08-13 13:18:49 +0100

    update

--- src/Strings/ReverseStringII.java
+++ src/Strings/ReverseStringII.java
@@ -1,10 +1,31 @@
 package Strings;
 
+import java.util.Arrays;
+
 public class ReverseStringII {
 
     //given a string, reverse it and remove all the leading and trailing spaces.
-
     // example: String str = " Aadhi programs in Java   "
-
     // result -> "Java in programs Aadhi"
+    public static void main(String[] args) {
+        String str = " Aadhi programs      in Java   ";
+        String [] word = str.trim().split(" ");
+        int N = word.length;
+        System.out.println("Original:");
+        System.out.println(Arrays.toString(word));
+        String [] word2 = Rev(word,N).trim().split(" ");
+        System.out.println("Reverse:");
+        System.out.println(Arrays.toString(word2));
+
+    }
+    static String Rev(String [] word, int N){
+        String rev = "";
+        for(int i = 0; i < N; i++){
+            if(word[i] == ""){
+                continue;
+            }
+            rev = word[i]+" "+ rev;
+        }
+        return rev;
+    }
 }
